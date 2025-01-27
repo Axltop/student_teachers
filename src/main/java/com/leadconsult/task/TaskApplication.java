c5c18624c8b8a7266c0b1d5118fc87ccffc4c838
axltop <alexandertopaloff@gmail.com>
2025-01-27 15:25:08 +0200

    add key vault

--- src/main/java/com/leadconsult/task/TaskApplication.java
+++ src/main/java/com/leadconsult/task/TaskApplication.java
@@ -1,13 +1,24 @@
 package com.leadconsult.task;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskApplication {
-
+public class TaskApplication implements CommandLineRunner {
+	// Spring Cloud Azure will automatically inject SecretClient in your ApplicationContext.
+	private final SecretClient secretClient;
+	public TaskApplication(SecretClient secretClient) {
+		this.secretClient = secretClient;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("h2url: " + secretClient.getSecret("h2url").getValue());
 	}
 
 }
